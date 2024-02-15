f06873743ced61e28b53c1f1453dba756e812495
viman39 <vimanmanuel@gmail.com>
2024-02-15 22:12:19 +0200

    add layout home

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,16 @@
+import "./dashboard.scss";
+
 export const Dashboard = () => {
   return (
     <>
-      <h3 style={{ marginTop: "0px" }}>Lorem Ipsum</h3>
-      <section style={{ textAlign: "justify" }}>
-        <p>
+      <div className="home-section">
+        <h1>LespeziArt Con</h1> <br />
+        <span>
+          Izolații care rezistă timpului, la înălțimea așteptărilor tale!
+        </span>
+      </div>
+      <div className="aboutus-section">
+        <div>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod
           dolor sed turpis lacinia suscipit. Integer porta vitae erat in
           blandit. In in ipsum semper, volutpat leo sed, scelerisque purus. Orci
@@ -17,8 +24,8 @@ export const Dashboard = () => {
           eleifend faucibus eget ut velit. Cras sollicitudin scelerisque magna
           et faucibus. Aliquam sit amet porta ipsum, sed hendrerit quam. Nunc
           iaculis massa ut metus convallis, at eleifend nunc tincidunt.
-        </p>
-        <p>
+        </div>
+        <div>
           Morbi varius hendrerit quam ut blandit. Morbi est lectus, consectetur
           in erat nec, maximus euismod justo. Morbi sollicitudin lectus sed
           augue lacinia ultricies. Maecenas euismod tellus id urna posuere,
@@ -30,8 +37,8 @@ export const Dashboard = () => {
           finibus leo, quis malesuada lectus ante tempus dolor. Vivamus aliquet
           egestas neque ac porta. Integer eu diam iaculis est facilisis gravida.
           Sed sit amet tincidunt orci.
-        </p>
-        <p>
+        </div>
+        <div>
           Vivamus in odio consequat, malesuada diam ut, imperdiet lacus. Morbi
           eget augue enim. Nam at leo eleifend, lacinia ante sed, rhoncus
           mauris. Sed feugiat dolor id ullamcorper laoreet. Etiam a sem quis
@@ -40,8 +47,46 @@ export const Dashboard = () => {
           ante, et tincidunt nisi convallis eget. Donec a libero nisl. Nullam
           magna est, vehicula at luctus eget, finibus eget mi. Aenean interdum
           velit id nibh posuere porttitor.
-        </p>
-      </section>
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod
+          dolor sed turpis lacinia suscipit. Integer porta vitae erat in
+          blandit. In in ipsum semper, volutpat leo sed, scelerisque purus. Orci
+          varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Sed ex est, iaculis eu feugiat in, interdum id sapien.
+          Etiam vitae laoreet leo, non viverra massa. Curabitur et facilisis
+          libero, nec eleifend sem. Fusce ultrices a augue in imperdiet. Nulla
+          bibendum, metus nec pulvinar bibendum, diam lectus pulvinar ipsum, ac
+          ultrices tellus mi quis metus. Ut massa erat, dictum sollicitudin
+          bibendum tincidunt, venenatis vel dolor. Donec quis nisl sit amet mi
+          eleifend faucibus eget ut velit. Cras sollicitudin scelerisque magna
+          et faucibus. Aliquam sit amet porta ipsum, sed hendrerit quam. Nunc
+          iaculis massa ut metus convallis, at eleifend nunc tincidunt.
+        </div>
+        <div>
+          Morbi varius hendrerit quam ut blandit. Morbi est lectus, consectetur
+          in erat nec, maximus euismod justo. Morbi sollicitudin lectus sed
+          augue lacinia ultricies. Maecenas euismod tellus id urna posuere,
+          blandit bibendum elit convallis. Vivamus nisi nulla, bibendum at
+          tristique eget, consequat nec velit. Sed non congue nibh. Quisque sit
+          amet pharetra lorem, a ornare lacus. Praesent sit amet dui et nunc
+          tristique mollis. Aliquam bibendum accumsan odio, in tincidunt massa
+          interdum non. Integer dapibus, est id mattis gravida, dui velit
+          finibus leo, quis malesuada lectus ante tempus dolor. Vivamus aliquet
+          egestas neque ac porta. Integer eu diam iaculis est facilisis gravida.
+          Sed sit amet tincidunt orci.
+        </div>
+        <div>
+          Vivamus in odio consequat, malesuada diam ut, imperdiet lacus. Morbi
+          eget augue enim. Nam at leo eleifend, lacinia ante sed, rhoncus
+          mauris. Sed feugiat dolor id ullamcorper laoreet. Etiam a sem quis
+          orci bibendum finibus. In gravida luctus nisi quis blandit. Maecenas
+          ornare interdum libero, eget cursus ipsum. Integer dapibus libero
+          ante, et tincidunt nisi convallis eget. Donec a libero nisl. Nullam
+          magna est, vehicula at luctus eget, finibus eget mi. Aenean interdum
+          velit id nibh posuere porttitor.
+        </div>
+      </div>
     </>
   );
 };
